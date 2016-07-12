f2af4a94ca0cada10e56332185266b5bc1dde5b1
Joshua Jolley <jolleyboy@gmail.com>
2016-07-12 15:30:31 -0600

    Updated vastness.  We found the solution "

--- algorithms/vastness/src/vastness/part2.clj
+++ algorithms/vastness/src/vastness/part2.clj
@@ -1,8 +1,8 @@
 (ns vastness.part2
   (:require [clojure.test :refer :all]
-            [clojure.string :refer [join]])
+            [clojure.string :refer [join]]
+            [clojure.math.combinatorics :as combo])
   (:gen-class))
-
 
 (defn get-energy
   "Fetches the energy for a sequence of chars from Br. Neff's server."
@@ -27,42 +27,32 @@
                (Math/exp (- (/ (- new-energy energy) temperature))))]
     (if (> prob (rand)) [new-state new-energy] [state energy])))
 
+(defn swap [s i j]
+  (let [v (vec s)]
+    (apply str (assoc v j (v i) i (v j)))))
+
 (defn state->next-state
   "Return a new state based on a manipulation "
   [[chars energy]] 
   [(apply str (swap (seq chars) (rand-int (count chars)) (rand-int (count chars)))) energy])
 
 
-(defn get-permutation[chars wall char]
-  (let [index (.indexOf (vec chars) char)
-        new-chars (swap (seq chars) )
-        ] ;;get the index of the char in the substring
-    
-    (print index)
-    ))
+(defn get-permutation[chars wall index]
+  (let [new-chars (swap (seq chars) wall index)]
+    (get-energy [new-chars 0])))
 
-(get-permutation "tesing" 0 \s)
 
 (defn smart-state-swap
   "Solve a single letter in the string"
   [[chars energy] wall]
-  (let [substring (subs chars wall) ;;get a substring from wall to end of chars
+  (let [substring (subs chars (inc wall)) ;;get a substring from wall to end of chars
+        wall-char (.charAt chars wall)
         prefix (subs chars 0 wall)
-        char-permutations (pmap identity substring) ;;create a list of states from every char in the substring as the char after wall
-        ;;return the lowest energy as the next state.
+        indices (map #(+ (inc wall) %) (range (count substring)))
+        permutations (pmap #(get-permutation chars wall %) indices)]
+    (println permutations)
+    (doto (first (sort-by second permutations)) println))) 
 
-
-        ]
-    (prn prefix substring)
-    )
-  )
-
-(smart-state-swap ["abcdefghijklmnopqrst" 100000000] 1)
-
-
-(defn swap [s i j]
-  (let [v (vec s)]
-    (apply str (assoc v j (v i) i (v j)))))
 
 (defn get-best-state [[best-state best-energy] [new-state new-energy]]
   (if (< new-energy best-energy)[new-state new-energy][best-state best-energy]))
@@ -70,62 +60,45 @@
 (defn simulated-annealing [state temp cooling-rate] 
   (loop [v {:state (get-energy state)
             :best-state (get-energy state) 
-            :temp temp}]
+            :temp temp
+            :wall 0}]
     (if (<= (:temp v) 1)
       (doto (:best-state v) (println (chars->sentence (first (:best-state v)))))
-      (let [temp-state (get-energy (state->next-state (:state v)))
+      (let [temp-state (get-energy (smart-state-swap (:state v) (:wall v)))
             new-state (probability (:state v) temp-state (:temp v))
             new-best-state (get-best-state (:state v) new-state) 
-            new-temp (* (:temp v) cooling-rate)]
+            new-temp (* (:temp v) cooling-rate)
+            new-wall (if (= (inc (:wall v)) (dec (count (first (:state v))))) 0 (inc (:wall v)))]
+        
         (when (not= (:best-state v) new-best-state)
           (println  new-best-state " " (:temp v)))
         (recur (assoc v
                       :state new-state
                       :best-state new-best-state
-                      :temp new-temp) )))))
+                      :temp new-temp
+                      :wall new-wall))))))
 
 
-(spit "best_energies.txt" (str  (simulated-annealing ["bnhmpiacdefgjkloqrts"] 1000 0.995) "\n") :append true)
+;;(spit "best_energies.txt" (str  (simulated-annealing ["abcdefghijklmnopqrst"] 1000 0.95) "\n") :append true)
 
-(chars->sentence "bnhmpicaedgfjkloqtrs" )
 
-(defn factorial [n] 
-  (reduce *' (range 1 (inc n))))
+;;(chars->sentence "bnhmpicaedgfjkloqtrs" )
+(defn factorial [n]
+  (reduce * (map inc (range n))))
 
-(factorial 3)
+(factorial 4)
 
-(defn myperm [xx nn]
-  (loop [x xx n nn result '()]
-    (if (empty? x)
-      (remove nil? result)
-      (let [fac (factorial (dec (count x)))
-            q (quot n fac)
-            r (mod n fac)
-            a (take q x)
-            c (first (drop q x))
-            b (rest (drop q x))]
-        (recur (into a b) r (cons c result))))))
+(defn rank [y]
+  (if (empty? y)
+    0
+    (let [x0 (int (first y))
+          xs (rest y)]
+      (+ (rank xs)
+         (* (count (for [x xs :when (< (int x) x0)] x))
+            (factorial (count xs)))))))
 
-(myperm '(1 2 3 4 5) 0)
+(get-energy ["abcdefghijklmnopqrst" 0])
 
-(myperm [1 2 3 4 5] 1)
+(rank (vec "abcdefghijklmnopqrst"))
 
-(map inc (range 5))
-
-(defn testidea [n]
-  (let [lst (map inc (range n))
-        perms (map #(myperm lst %) (range (factorial n)))
-        dperms (distinct perms)]
-    (println perms "\n" dperms)
-    (= perms dperms)))
-
-(myperm [1 2 3] 7)
-
-(testidea 3)
-
-(map #(myperm [1 2 3 4] %) (range (dec (factorial 4))))
-
-(take 2 '(1 2 3 4 5))
-(drop 2 '(1 2 3 4 5))
-
-(myperm '(1 2 3) 0)
+(chars->sentence (first (get-energy [(apply str (combo/nth-permutation "abcdefghijklmnopqrst" 200831837313463612)) 0])))
